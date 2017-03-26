68eae7697dca7db539456e8b915c0debfa383adf
ted94vip <fedos94vip@mail.ru>
2017-03-26 18:34:27 +0300

    вывод содержимого корзины в виде сохранение ид каждого товара и вывод их в заказ отправка оповещение администратору сайта и пользователю информацию о новом заказе

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -5,13 +5,12 @@
  * Date: 19.03.2017
  * Time: 16:21
  */
-
 namespace app\controllers;
 use app\models\Product;
 use app\models\Cart;
+use app\models\Order;
+use app\models\OrderItems;
 use Yii;
-
-
 class CartController extends AppController
 {
     public function actionAdd()
@@ -30,9 +29,7 @@ class CartController extends AppController
         }
         $this->layout=false;
         return$this->render('cart-modal',compact('session'));
-
     }
-
     public function actionClear()
     {
         $session=Yii::$app->session;
@@ -42,7 +39,6 @@ class CartController extends AppController
         $session->remove('cart.sum');
         $this->layout=false;
         return$this->render('cart-modal',compact('session'));
-
     }
     public function actionDelItem(){
         $id=Yii::$app->request->get('id');
@@ -53,13 +49,51 @@ class CartController extends AppController
         $this->layout=false;
         return$this->render('cart-modal',compact('session'));
     }
-     public function actionShow(){
-         $session=Yii::$app->session;
-         $session->open();
-         $this->layout=false;
-         return$this->render('cart-modal',compact('session'));
-     }
+    public function actionShow(){
+        $session=Yii::$app->session;
+        $session->open();
+        $this->layout=false;
+        return$this->render('cart-modal',compact('session'));
+    }
     public function actionView(){
-        return $this->render('view');
+        $session=Yii::$app->session;
+        $session->open();
+        $this->setMeta('Корзина');
+        $order= new Order();
+        if($order->load(Yii::$app->request->post())){
+            $order->qty=$session['cart.qty'];
+            $order->sum=$session['cart.sum'];
+            if($order->save()){
+                $this->saveOrderItems($session['cart'],$order->id);
+                Yii::$app->session->setFlash('success','Ваш заказ принят!');
+                Yii::$app->mailer->compose('order', ['session' => $session])
+                   ->setFrom(['top_top_md@mail.ru'=> 'top-top.md'])
+                   ->setTo([$order->email,Yii::$app->params['adminEmail']])
+                   ->setSubject('Заказ')
+                   ->send();
+                $session->remove('cart');
+                $session->remove('cart.qty');
+                $session->remove('cart.sum');
+                return $this->refresh();
+            }
+            else{
+                Yii::$app->session->setFlash('error','Ошибка оформаления заказа!');
+            }
+
+        }
+        return $this->render('view',compact('session','order'));
+    }
+   protected  function saveOrderItems($items, $order_id){
+      foreach($items as $id=>$item){
+          $order_items = new OrderItems();
+          $order_items->order_id=$order_id;
+          $order_items->product_id=$id;
+          $order_items->name=$item['name'];
+          $order_items->price=$item['price'];
+          $order_items->qty_item=$item['qty'];
+          $order_items->sum_item=$item['qty']*$item['price'];
+           $order_items->save();
+
+      }
     }
 }
