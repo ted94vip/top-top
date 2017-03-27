674e63bc633d41355a31983d5d55be5e73c45ca5
ted94vip <fedos94vip@mail.ru>
2017-03-27 14:38:51 +0300

    авторизация по email

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -13,7 +13,7 @@ use yii\base\Model;
  */
 class LoginForm extends Model
 {
-    public $username;
+    public $email;
     public $password;
     public $rememberMe = true;
 
@@ -27,10 +27,11 @@ class LoginForm extends Model
     {
         return [
             // username and password are both required
-            [['username', 'password'], 'required'],
+            [['username', 'password','email'], 'required'],
             // rememberMe must be a boolean value
             ['rememberMe', 'boolean'],
-            // password is validated by validatePassword()
+            [['email'], 'email'],
+                // password is validated by validatePassword()
             ['password', 'validatePassword'],
         ];
     }
@@ -73,7 +74,7 @@ class LoginForm extends Model
     public function getUser()
     {
         if ($this->_user === false) {
-            $this->_user = User::findByUsername($this->username);
+            $this->_user = User::findByEmail($this->email);
         }
 
         return $this->_user;
