9b4b6014ce31cbb2b8d40687b3feeb9fb99937a2
ted94vip <fedos94vip@mail.ru>
2017-04-08 20:51:29 +0300

    редактирование вида регистрации

--- views/site/signup.php
+++ views/site/signup.php
@@ -12,7 +12,8 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="col-lg-5">
 
             <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
-            <?= $form->field($model, 'login')->textInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
             <?= $form->field($model, 'email') ?>
             <?= $form->field($model, 'password')->passwordInput() ?>
             <div class="form-group">
@@ -23,4 +24,5 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
 </div>
+
 
